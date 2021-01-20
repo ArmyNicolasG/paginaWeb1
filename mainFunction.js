dfc5cd657de1513a748e59aaede1b4335863b6a5
Your Name <you@example.com>
2021-01-20 14:39:57 -0500

    1.5

---
+++ mainFunction.js
@@ -0,0 +1,6 @@
+var confirmadorSuma = document.getElementById('buttonSuma')
+confirmadorSuma.addEventListener("click", () => {
+    let num1Suma = parseInt(document.getElementById('suma1')).value;
+    let num2Suma = parseInt(document.getElementById('suma2')).value;
+    console.log(num1Suma + num2Suma)
+})
