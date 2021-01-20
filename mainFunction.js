2fc6dd78da94f0547eb80b2bcfffd292b3e6d34a
Your Name <you@example.com>
2021-01-20 14:46:27 -0500

    1.7

--- mainFunction.js
+++ mainFunction.js
@@ -2,5 +2,26 @@ var confirmadorSuma = document.getElementById('buttonSuma')
 confirmadorSuma.addEventListener("click", () => {
     let num1Suma = parseInt(document.getElementById('suma1').value);
     let num2Suma = parseInt(document.getElementById('suma2').value);
-    console.log(num1Suma + num2Suma)
+    document.getElementById('resSum').innerHTML = num1Suma + num2Suma
 })
+var confirmadorSuma = document.getElementById('buttonSuma')
+confirmadorSuma.addEventListener("click", () => {
+    let num1Suma = parseInt(document.getElementById('suma1').value);
+    let num2Suma = parseInt(document.getElementById('suma2').value);
+    document.getElementById('resSum').innerHTML = num1Suma + num2Suma
+})
+
+var confirmadorSuma = document.getElementById('buttonSuma')
+confirmadorSuma.addEventListener("click", () => {
+    let num1Suma = parseInt(document.getElementById('suma1').value);
+    let num2Suma = parseInt(document.getElementById('suma2').value);
+    document.getElementById('resSum').innerHTML = num1Suma + num2Suma
+})
+
+var confirmadorSuma = document.getElementById('buttonSuma')
+confirmadorSuma.addEventListener("click", () => {
+    let num1Suma = parseInt(document.getElementById('suma1').value);
+    let num2Suma = parseInt(document.getElementById('suma2').value);
+    document.getElementById('resSum').innerHTML = num1Suma + num2Suma
+})
+
